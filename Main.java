00e5afc0fa1d54fc5e971d9c0c64ffcb9e9e7e3f
spark8921 <169144373+spark8921@users.noreply.github.com>
2024-05-07 15:44:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        toUppercase("Hello, World!");
+       String str="null";
+       System.out.println(toUpperCase(str));
     }
-    public static void toUppercase(String str) {
-        String result = "";
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (Character.isLetter(ch) && Character.isLowerCase(ch)) {
-                result += Character.toUpperCase(ch);
-            } else {
-                result += ch;
+    public static String toUpperCase(String str){
+        String res="";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>=97&&ch<=122){
+                ch-=32;
+                res+=ch;
+            }
+            else{
+                res+=ch;
             }
         }
-        System.out.println(result);
+        return res;
     }
 }
